2d07b4255f64544b944e44aa9402ac5ece5bc927
Joshua Thomas <134388878+Joshuathomas22135@users.noreply.github.com>
2026-09-21 19:27:24 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+# A Python program that demonstrates key XOR rules by showing how XOR with 0 keeps a number unchanged, XOR 
+# with itself gives 0, and repeated values cancel each other out.
+
+a = 12
+b = 16
+
+print(a ^ 0)
+print(a ^ a)
+print(a ^ b)
